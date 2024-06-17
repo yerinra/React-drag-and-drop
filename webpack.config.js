cce80514904cc590a4520ae1cc55c87c695142f9
yerinra <yerinaleph@gmail.com>
2024-06-17 12:58:00 +0900

    build: 바벨 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -11,6 +12,16 @@ module.exports = {
     rules: [
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
       { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: "asset/resource" },
+      {
+        test: /.m?js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
     ],
   },
 };
