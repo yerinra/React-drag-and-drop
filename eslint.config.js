94fb94aa1d270f656d85ff07c0ee47f448b2a4d1
yerinra <yerinaleph@gmail.com>
2024-06-17 13:53:50 +0900

    chore: eslint 및 prettier 설정

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
