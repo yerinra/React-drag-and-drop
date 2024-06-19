12427c922689516e8768360a186ab36058845ac1
yerinra <yerinaleph@gmail.com>
2024-06-19 18:20:16 +0900

    feat: 헤더 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header({ title, children }) {
   return (
     <header className="text-center mx-auto my-10">
       <h1 className="text-3xl font-bold mb-2">{title}</h1>
-      <div className="flex flex-col">{children}</div>
+      <span className="text-gray-400 text-sm">{children}</span>
     </header>
   );
 }
