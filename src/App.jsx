59ad58fe50058b21ba0d69818865a414040ea0f7
yerinra <yerinaleph@gmail.com>
2024-06-19 14:39:24 +0900

    feat: MultiDragAndDrop 컴포넌트에 초기 데이터 전달

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 import Header from "./components/Header";
 import MultiDragAndDrop from "./components/MultiDragAndDrop";
+import initial from "./lib/data";
 
 export default function App() {
   return (
-    <div className="flex flex-col p-4 h-dvh">
-      <Header title="Drag and Drop">자유롭게 드래그해서 움직여보세요!✨</Header>
+    <div className="flex flex-col p-4 h-full">
+      <Header title="Drag and Drop">
+        자유롭게 선택하고 드래그 해보세요!✨
+      </Header>
 
       <main className="flex justify-center gap-20 mt-4">
-        <MultiDragAndDrop />
+        <MultiDragAndDrop data={initial} />
       </main>
     </div>
   );
