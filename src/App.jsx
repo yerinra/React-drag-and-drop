6591fbdcc7a006712b3663f793ffc8c136bfcd77
yerinra <yerinaleph@gmail.com>
2024-06-19 16:17:02 +0900

    feat: 헤더 수정

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,16 @@ export default function App() {
   return (
     <div className="flex flex-col p-4 h-full">
       <Header title="Drag and Drop">
-        자유롭게 선택하고 드래그 해보세요!✨
+        <span className="text-gray-400 text-sm">
+          자유롭게 선택하고 드래그 해보세요!✨
+        </span>
+        <ul className="mt-4 text-start text-red-400 text-sm">
+          <li>One에 있는 아이템을 Three로 옮길 수 없습니다.</li>
+          <li>짝수 아이템 앞에 짝수 아이템을 놓을 수 없습니다.</li>
+        </ul>
       </Header>
 
-      <main className="flex justify-center gap-20 mt-4">
+      <main className="flex justify-center gap-10 mt-4">
         <MultiDragAndDrop data={initial} />
       </main>
     </div>
